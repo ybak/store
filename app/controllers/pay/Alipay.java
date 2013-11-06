f21775880a8c0f4813f60ae332465206348d36e9
ybak.yf <ybakswu@gmail.com>
2013-11-06 18:58:30 +0800

    add alipay notify and callback.

--- app/controllers/pay/Alipay.java
+++ app/controllers/pay/Alipay.java
@@ -2,9 +2,14 @@ package controllers.pay;
 
 import java.net.URLDecoder;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+
 import modules.pay.config.AlipayConfig;
+import modules.pay.util.AlipayNotify;
 import modules.pay.util.AlipaySubmit;
 import play.mvc.Controller;
 
@@ -84,7 +89,7 @@ public class Alipay extends Controller {
         sHtmlTextToken = URLDecoder.decode(sHtmlTextToken, AlipayConfig.input_charset);
         // 获取token
         String request_token = AlipaySubmit.getRequestToken(sHtmlTextToken);
-        // out.println(request_token);
+        // response.print(request_token);
 
         // //////////////////////////////////根据授权码token调用交易接口alipay.wap.auth.authAndExecute//////////////////////////////////////
 
@@ -107,9 +112,129 @@ public class Alipay extends Controller {
         String sHtmlText = AlipaySubmit.buildRequest(ALIPAY_GATEWAY_NEW, sParaTemp, "get", "确认");
         response.print(sHtmlText);
     }
-    
-    public void nofity(){
-        
+
+    public static void callback() throws Exception {
+        // 获取支付宝GET过来反馈信息
+        Map<String, String> params = new HashMap<String, String>();
+        Map requestParams = request.params.data;
+        for (Iterator iter = requestParams.keySet().iterator(); iter.hasNext();) {
+            String name = (String) iter.next();
+            String[] values = (String[]) requestParams.get(name);
+            String valueStr = "";
+            for (int i = 0; i < values.length; i++) {
+                valueStr = (i == values.length - 1) ? valueStr + values[i] : valueStr + values[i] + ",";
+            }
+            // 乱码解决，这段代码在出现乱码时使用。如果mysign和sign不相等也可以使用这段代码转化
+            valueStr = new String(valueStr.getBytes("ISO-8859-1"), "utf-8");
+            params.put(name, valueStr);
+        }
+
+        // 获取支付宝的通知返回参数，可参考技术文档中页面跳转同步通知参数列表(以下仅供参考)//
+        // 商户订单号
+
+        String out_trade_no = new String(request.params.get("out_trade_no").getBytes("ISO-8859-1"), "UTF-8");
+
+        // 支付宝交易号
+
+        String trade_no = new String(request.params.get("trade_no").getBytes("ISO-8859-1"), "UTF-8");
+
+        // 交易状态
+        String result = new String(request.params.get("result").getBytes("ISO-8859-1"), "UTF-8");
+
+        // 获取支付宝的通知返回参数，可参考技术文档中页面跳转同步通知参数列表(以上仅供参考)//
+
+        // 计算得出通知验证结果
+        boolean verify_result = AlipayNotify.verifyReturn(params);
+
+        if (verify_result) {// 验证成功
+            // ////////////////////////////////////////////////////////////////////////////////////////
+            // 请在这里加上商户的业务逻辑程序代码
+
+            // ——请根据您的业务逻辑来编写程序（以下代码仅作参考）——
+
+            // 判断该笔订单是否在商户网站中已经做过处理
+            // 如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
+            // 如果有做过处理，不执行商户的业务程序
+
+            // 该页面可做页面美工编辑
+            response.print("验证成功<br />");
+            // ——请根据您的业务逻辑来编写程序（以上代码仅作参考）——
+
+            // ////////////////////////////////////////////////////////////////////////////////////////
+        } else {
+            // 该页面可做页面美工编辑
+            response.print("验证失败");
+        }
+    }
+
+    public static void nofity() throws Exception {
+        // 获取支付宝POST过来反馈信息
+        Map<String, String> params = new HashMap<String, String>();
+        Map requestParams = request.params.data;
+        for (Iterator iter = requestParams.keySet().iterator(); iter.hasNext();) {
+            String name = (String) iter.next();
+            String[] values = (String[]) requestParams.get(name);
+            String valueStr = "";
+            for (int i = 0; i < values.length; i++) {
+                valueStr = (i == values.length - 1) ? valueStr + values[i] : valueStr + values[i] + ",";
+            }
+            // 乱码解决，这段代码在出现乱码时使用。如果mysign和sign不相等也可以使用这段代码转化
+            // valueStr = new String(valueStr.getBytes("ISO-8859-1"), "gbk");
+            params.put(name, valueStr);
+        }
+
+        // 获取支付宝的通知返回参数，可参考技术文档中页面跳转同步通知参数列表(以下仅供参考)//
+
+        // 解密（如果是RSA签名需要解密，如果是MD5签名则下面一行清注释掉）
+        Map<String, String> decrypt_params = AlipayNotify.decrypt(params);
+        // XML解析notify_data数据
+        Document doc_notify_data = DocumentHelper.parseText(decrypt_params.get("notify_data"));
+
+        // 商户订单号
+        String out_trade_no = doc_notify_data.selectSingleNode("//notify/out_trade_no").getText();
+
+        // 支付宝交易号
+        String trade_no = doc_notify_data.selectSingleNode("//notify/trade_no").getText();
+
+        // 交易状态
+        String trade_status = doc_notify_data.selectSingleNode("//notify/trade_status").getText();
+
+        // 获取支付宝的通知返回参数，可参考技术文档中页面跳转同步通知参数列表(以上仅供参考)//
+
+        if (AlipayNotify.verifyNotify(params)) {// 验证成功
+            // ////////////////////////////////////////////////////////////////////////////////////////
+            // 请在这里加上商户的业务逻辑程序代码
+
+            // ——请根据您的业务逻辑来编写程序（以下代码仅作参考）——
+
+            if (trade_status.equals("TRADE_FINISHED")) {
+                // 判断该笔订单是否在商户网站中已经做过处理
+                // 如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
+                // 如果有做过处理，不执行商户的业务程序
+
+                // 注意：
+                // 该种交易状态只在两种情况下出现
+                // 1、开通了普通即时到账，买家付款成功后。
+                // 2、开通了高级即时到账，从该笔交易成功时间算起，过了签约时的可退款时限（如：三个月以内可退款、一年以内可退款等）后。
+
+                response.print("success"); // 请不要修改或删除
+            } else if (trade_status.equals("TRADE_SUCCESS")) {
+                // 判断该笔订单是否在商户网站中已经做过处理
+                // 如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
+                // 如果有做过处理，不执行商户的业务程序
+
+                // 注意：
+                // 该种交易状态只在一种情况下出现——开通了高级即时到账，买家付款成功后。
+
+                response.print("success"); // 请不要修改或删除
+            }
+
+            // ——请根据您的业务逻辑来编写程序（以上代码仅作参考）——
+
+            // ////////////////////////////////////////////////////////////////////////////////////////
+        } else {// 验证失败
+            response.print("fail");
+        }
     }
 
 }
